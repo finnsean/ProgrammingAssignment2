ad2d1ee333954ff1cefd7f7edd466aa97fdee109
finnsean <finnseanm@gmail.com>
2017-04-05 08:48:34 +0100

    minor update

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 ## A function which constructs on object consisting of a list of functions:
 ## 1) set the value of the matrix
 ## 2) get the value of the matrix
+## 3) set the value of the matrix inverse
 ## 4) get the value of the matrix inverse
 ## 
 ## A call to makeCacheMatrix() or set() associates an R matrix object with
@@ -22,22 +23,34 @@ makeCacheMatrix <- function(x = matrix()) {
   ## a function to return the matrix
   get <- function() x
   
-  ## a function to get the inverse matrix
-  ##getinv <- function() xInv
-  getinv <- function(...) {
-    if (!is.null(xInv)) {
-      message("Returning a cached inverse matrix")
-      return(xInv)
-    }
-    ## if exectution reaches here, no inverse exists and so one must be computed
-    xInv <<- solve(x,...) ## compute the inverse, store in parent environment
-    ## return the inverse
-    xInv
+  ## a function to set the inverse matrix
+  setinv <- function(inv) xInv <<- inv
   
-  }
+  ## a function to get the inverse matrix
+  getinv <- function() xInv
   
   ## compute a list of functions to be returned on 'construction'
   list(set = set, get = get,
-       getinv = getinv)
+       getinv = getinv,
+       setinv = setinv)
   
+}
+
+## This function calculates the inverse of the special "matrix" created 
+## with the above function. 
+## A call to this function computes the inverse matrix, but if 
+## an inverse has already been computed and chached, this value
+## is returned instead.
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
